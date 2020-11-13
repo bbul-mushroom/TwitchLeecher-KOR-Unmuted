aa3c97246272c80de0dfb56e8ada178894499778
bbul-mushroom <syudal@naver.com>
2020-11-13 12:09:05 +0900

    bump version 1.8.2 Unmuted Ver

--- TwitchLeecher/GlobalAssemblyInfo.cs
+++ TwitchLeecher/GlobalAssemblyInfo.cs
@@ -2,9 +2,9 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("Twitch Leecher")]
-[assembly: AssemblyDescription("트위치 영상 & 클립 다운로더")]
-[assembly: AssemblyCompany("Franiac & Syudal")]
-[assembly: AssemblyCopyright("Copyright ?2018 Dominik Rebitzer & Syudal")]
+[assembly: AssemblyDescription("트위치 영상 & 클립 다운로더 - 음소거제거버전")]
+[assembly: AssemblyCompany("Franiac & Syudal & Bbul-Mushroom")]
+[assembly: AssemblyCopyright("Copyright ?2018 Dominik Rebitzer & Syudal & Bbul-Mushroom")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.8.2")]
 [assembly: ComVisible(false)]
